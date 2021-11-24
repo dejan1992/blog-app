445d98f82819e43b5799a47ca03ecb57651e952b
Dejan Cetojevic <dejan92ceta@live.com>
2021-11-24 18:58:28 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+
 
 const App = () => {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Navbar />
+      <Hero />
+      <Main />
+      <Footer />
+
     </div>
   )
 }
